045aaf89e1d1e294d88bf1726e1059ea5ceaed47
Alabbas Alhaj Ali <weeamha@gmail.com>
2018-11-30 08:22:42 +0100

    :art: Update and test Blank Analog Read example

--- examples/Blink_AnalogRead/Blink_AnalogRead.ino
+++ examples/Blink_AnalogRead/Blink_AnalogRead.ino
@@ -4,34 +4,28 @@
 void TaskBlink( void *pvParameters );
 void TaskAnalogRead( void *pvParameters );
 
-// the setup function runs once when you press reset or power the board
+// the setup function runs once when 
+// you press reset or power the board
 void setup() {
   
-  // initialize serial communication at 9600 bits per second:
+  // initialize serial communication 
+  // at 9600 bits per second:
   Serial.begin(9600);
   
   while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB, on LEONARDO, MICRO, YUN, and other 32u4 based boards.
+    ; // wait for serial port to connect. 
+    // Needed for native USB, on LEONARDO, MICRO, YUN, and other 32u4 based boards.
   }
 
   // Now set up two tasks to run independently.
-  xTaskCreate(
-    TaskBlink
-    ,  (const portCHAR *)"Blink"   // A name just for humans
-    ,  128  // This stack size can be checked & adjusted by reading the Stack Highwater
-    ,  NULL
-    ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
-    ,  NULL );
+  xTaskCreate(TaskBlink, (const portCHAR *) "Blink", 128, NULL, 2, NULL);   
+  // "Blink" A name just for humans
+  // 128 This stack size can be checked & adjusted by reading the Stack Highwater
+  // 2 Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
 
-  xTaskCreate(
-    TaskAnalogRead
-    ,  (const portCHAR *) "AnalogRead"
-    ,  128  // Stack size
-    ,  NULL
-    ,  1  // Priority
-    ,  NULL );
-
-  // Now the task scheduler, which takes over control of scheduling individual tasks, is automatically started.
+  xTaskCreate(TaskAnalogRead, (const portCHAR *) "AnalogRead", 128, NULL, 1, NULL);
+  // Now the task scheduler, which takes over control
+  // of scheduling individual tasks, is automatically started.
 }
 
 void loop()
@@ -43,9 +37,31 @@ void loop()
 /*---------------------- Tasks ---------------------*/
 /*--------------------------------------------------*/
 
-void TaskBlink(void *pvParameters)  // This is a task.
-{
+void TaskBlink(void *pvParameters) {
   (void) pvParameters;
+  // initialize digital LED_BUILTIN on pin 13 as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // A Task shall never return or exit.
+  for (;;) {
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
+  }
+}
+
+void TaskAnalogRead(void *pvParameters) {
+  (void) pvParameters;
+  for (;;) {
+    // read the input on analog pin 0:
+    int sensorValue = analogRead(A0);
+    // print out the value you read:
+    Serial.println(sensorValue);
+    vTaskDelay(1);  // one tick delay (15ms) in between reads for stability
+  }
+}
+
 
 /*
   Blink
@@ -71,22 +87,6 @@ void TaskBlink(void *pvParameters)  // This is a task.
   by Arturo Guadalupi
 */
 
-  // initialize digital LED_BUILTIN on pin 13 as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-
-  for (;;) // A Task shall never return or exit.
-  {
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-    vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
-    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-    vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
-  }
-}
-
-void TaskAnalogRead(void *pvParameters)  // This is a task.
-{
-  (void) pvParameters;
-  
 /*
   AnalogReadSerial
   Reads an analog input on pin 0, prints the result to the serial monitor.
@@ -95,13 +95,3 @@ void TaskAnalogRead(void *pvParameters)  // This is a task.
 
   This example code is in the public domain.
 */
-
-  for (;;)
-  {
-    // read the input on analog pin 0:
-    int sensorValue = analogRead(A0);
-    // print out the value you read:
-    Serial.println(sensorValue);
-    vTaskDelay(1);  // one tick delay (15ms) in between reads for stability
-  }
-}
